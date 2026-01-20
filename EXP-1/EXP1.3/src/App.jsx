e5e27a556a0f65a9969f9ba87d2ed374a2bd6674
Satyam Gupta <satym0696@gmail.com>
2026-01-20 12:13:39 +0530

    Resolved merge conflicts

--- EXP-1/EXP1.3/src/App.jsx
+++ EXP-1/EXP1.3/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
         </p>
 
         <p className="info-text">
-          This is the 3 part of first experimental lab activity done in 23aml-2(a) FSD-2 lab.
+          This is the 3 part of first experimental lab activity done in 23aml-4(b) FSD-2 lab.
         </p>
         <p>
           -----------------------------------------------------
