a2f2be5732b7a459bcf1771c5c0fc20330c01701
Satyam Gupta <satym0696@gmail.com>
2026-02-24 12:24:31 +0530

    Remove submodules and track all project folders normally

---
+++ Experiment-5/src/User.jsx
@@ -0,0 +1,15 @@
+function User() {
+  return (
+    <div>
+      <h1>User name is Satyam Gupta</h1>
+      <h1>User name is Satyam Gupta</h1>
+      <h1>User name is Satyam Gupta</h1>
+      <h1>User name is Satyam Gupta</h1>
+      <h1>User name is Satyam Gupta</h1>
+      <h1>User name is Satyam Gupta</h1>
+      <h1>User name is Satyam Gupta</h1>
+    </div>
+  );
+}
+
+export default User;
